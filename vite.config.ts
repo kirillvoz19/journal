61de912e1feac7e58195edb5a113402857e2bf8e
Your Name <you@example.com>
2026-02-12 14:12:51 +0300

    deploy to github actions

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/journal/',
   build: {
     outDir: 'dist',
   },
